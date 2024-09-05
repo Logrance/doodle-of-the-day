72e42d3fd76291c5aad7bb4b0cfecbecbd3b53c9
Max Wellings <maxmechlin@gmail.com>
2024-09-05 23:46:11 +0100

    canvas implemented

--- screens/HomeScreen/(tabs)/CanvasScreen.tsx
+++ screens/HomeScreen/(tabs)/CanvasScreen.tsx
@@ -1,18 +1,265 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Dimensions, TouchableOpacity, StyleSheet } from 'react-native';
+import { Gesture, GestureDetector, GestureHandlerRootView} from 'react-native-gesture-handler'
+import { Canvas, Circle, Path, Skia, ImageSVG } from '@shopify/react-native-skia';
+import Animated, {useSharedValue, withTiming, useAnimatedStyle, withSpring } from 'react-native-reanimated';
+import { Ionicons, FontAwesome5 } from '@expo/vector-icons';
+import { StatusBar } from 'expo-status-bar';
+
+interface IPath {
+  segments: String[];
+  color?: string;
+}
+
+interface ICircle {
+  x: number;
+  y: number;
+}
+interface IStamp {
+  x: number;
+  y: number;
+  color: string;
+}
+
+enum Tools {
+  Pencil,
+  Stamp,
+}
+
 
 export default function CanvasScreen() {
+  const { width, height } = Dimensions.get("window");
+
+  const paletteColors = ["red", "green", "blue", "yellow"];
+
+  const svgStar =
+    '<svg class="star-svg" version="1.1" ……………..></polygon></svg>';
+
+  const [activePaletteColorIndex, setActivePaletteColorIndex] = useState(0);
+  const [activeTool, setActiveTool] = useState<Tools>(Tools.Pencil);
+  const [paths, setPaths] = useState<IPath[]>([]);
+  const [circles, setCircles] = useState<ICircle[]>([]);
+  const [stamps, setStamps] = useState<IStamp[]>([]);
+
+  //Defining a Pan Gesture
+  const pan = Gesture.Pan()
+    .runOnJS(true)
+    .onStart((g) => {
+      if (activeTool === Tools.Pencil) {
+        const newPaths = [...paths];
+        newPaths[paths.length] = {
+          segments: [],
+          color: paletteColors[activePaletteColorIndex],
+        };
+        newPaths[paths.length].segments.push(`M ${g.x} ${g.y}`);
+        setPaths(newPaths);
+      }
+    })
+    .onUpdate((g) => {
+      if (activeTool === Tools.Pencil) {
+        const index = paths.length - 1;
+        const newPaths = [...paths];
+        if (newPaths?.[index]?.segments) {
+          newPaths[index].segments.push(`L ${g.x} ${g.y}`);
+          setPaths(newPaths);
+        }
+      }
+    })
+    .onTouchesUp((g) => {
+      if (activeTool === Tools.Pencil) {
+        const newPaths = [...paths];
+        setPaths(newPaths);
+      }
+    })
+    .minDistance(1);
+
+    //defining a tap gesture
+    const tap = Gesture.Tap()
+    .runOnJS(true)
+    .onStart((g) => {
+      if (activeTool === Tools.Stamp) {
+        setStamps([
+          ...stamps,
+          {
+            x: g.x - 25,
+            y: g.y - 25,
+            color: paletteColors[activePaletteColorIndex],
+          },
+        ]);
+      }
+    });
+
+  const clearCanvas = () => {
+    setPaths([]);
+    setCircles([]);
+    setStamps([]);
+  };
+
+  //defining animated styles with UseAnimatedStyle
+  const paletteVisible = useSharedValue(false);
+  const animatedPaletteStyle = useAnimatedStyle(() => {
+    return {
+      top: withSpring(paletteVisible.value ? -275 : -100),
+      height: withTiming(paletteVisible.value ? 200 : 50),
+      opacity: withTiming(paletteVisible.value ? 100 : 0, { duration: 100 }),
+    };
+  });
+
+  const animatedSwatchStyle = useAnimatedStyle(() => {
+    return {
+      top: withSpring(paletteVisible.value ? -50 : 0),
+      height: paletteVisible.value ? 0 : 50,
+      opacity: withTiming(paletteVisible.value ? 0 : 100, { duration: 100 }),
+    };
+  });
+
+
+
   return (
-    <View style={styles.container}>
-      <Text>Canvas will be here</Text>
-    </View>
-  );
+    <>
+    <GestureHandlerRootView>
+      <View style={{ height, width }}>
+        <GestureDetector gesture={tap}>
+          <GestureDetector gesture={pan}>
+            <Canvas style={{ flex: 8 }}>
+              {circles.map((c, index) => (
+                <Circle key={index} cx={c.x} cy={c.y} r={10} />
+              ))}
+              {paths.map((p, index) => (
+                <Path
+                  key={index}
+                  path={p.segments.join(" ")}
+                  strokeWidth={5}
+                  style="stroke"
+                  color={p.color}
+                />
+              ))}
+              {stamps.map((s, index) => {
+                const image = Skia.SVG.MakeFromString(
+                  svgStar.replace("{{fillColor}}", s.color)
+                );
+                if (!image) return null;
+                return (
+                  <ImageSVG
+                    key={index}
+                    width={50}
+                    height={50}
+                    x={s.x}
+                    y={s.y}
+                    svg={image}
+                  />
+                );
+              })}
+            </Canvas>
+          </GestureDetector>
+        </GestureDetector>
+        <View style={{ padding: 10, flex: 1, backgroundColor: "#edede9" }}>
+          <View style={{ flex: 1, flexDirection: "row" }}>
+            <Animated.View
+              style={[
+                { padding: 10, position: "absolute", width: 60 },
+                animatedPaletteStyle,
+              ]}
+            >
+              {paletteColors.map((c, i) => (
+                <TouchableOpacity
+                  key={i}
+                  onPress={() => {
+                    setActivePaletteColorIndex(i);
+                    paletteVisible.value = false;
+                  }}
+                >
+                  <View
+                    style={[
+                      {
+                        backgroundColor: c,
+                      },
+                      styles.paletteColor,
+                    ]}
+                  ></View>
+                </TouchableOpacity>
+              ))}
+            </Animated.View>
+            <View style={styles.swatchContainer}>
+              <TouchableOpacity
+                onPress={() => {
+                  paletteVisible.value !== true
+                    ? (paletteVisible.value = true)
+                    : (paletteVisible.value = false);
+                }}
+              >
+                <Animated.View
+                  style={[
+                    {
+                      backgroundColor: paletteColors[activePaletteColorIndex],
+                    },
+                    styles.swatch,
+                    animatedSwatchStyle,
+                  ]}
+                />
+              </TouchableOpacity>
+              <View>
+                {activeTool === Tools.Pencil ? (
+                  <TouchableOpacity
+                    onPress={() => setActiveTool(Tools.Stamp)}
+                  >
+                    <FontAwesome5
+                      name="pencil-alt"
+                      style={styles.icon}
+                    ></FontAwesome5>
+                  </TouchableOpacity>
+                ) : (
+                  <TouchableOpacity
+                    onPress={() => setActiveTool(Tools.Pencil)}
+                  >
+                    <FontAwesome5
+                      name="stamp"
+                      style={styles.icon}
+                    ></FontAwesome5>
+                  </TouchableOpacity>
+                )}
+              </View>
+              <TouchableOpacity onPress={clearCanvas}>
+              <Ionicons
+                  name="md-trash-outline"
+                  style={styles.icon}
+                ></Ionicons>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </View>
+    </GestureHandlerRootView>
+    <StatusBar style="auto" />
+  </>
+);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+icon: {
+  fontSize: 40,
+  textAlign: "center",
+},
+paletteColor: {
+  width: 50,
+  height: 50,
+  borderRadius: 25,
+  marginVertical: 5,
+  zIndex: 2,
+},
+swatch: {
+  width: 50,
+  height: 50,
+  borderRadius: 25,
+  borderColor: "black",
+  marginVertical: 5,
+  zIndex: 1,
+},
+swatchContainer: {
+  flexDirection: "row",
+  flex: 1,
+  padding: 10,
+  justifyContent: "space-between",
+  alignItems: "center",
+},
 });
